15c2d0d83aecc498f0f606619cfdfd56a0b39c80
RobinDELEPINE <94934563+RobinDELEPINE@users.noreply.github.com>
2022-12-07 14:50:43 +0100

    Partie 1 du TP complétée à moitié

--- CPP_TP3.cpp
+++ CPP_TP3.cpp
@@ -3,8 +3,12 @@
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 #include <iostream>
+#include "point.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+    point<int> pt1(12, 13);
+    //point<int> pt2(pt1);
+
 }
---
+++ point.h
@@ -0,0 +1,62 @@
+#pragma once
+template<typename T>
+class point
+{
+protected:
+	int x = 0;
+	int y = 0;
+public :
+	point(T x, T y);
+	void translater(T a, T b);
+	point(point const & pt);
+	T getX();
+	T getY();
+	void setX(T a);
+	void setY(T b);
+};
+
+template<typename T>
+inline point<T>::point(T x, T y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+template<typename T>
+inline void point<T>::translater(T a, T b)
+{
+	this->x += a;
+	this->y += b;
+}
+
+template<typename T>
+inline point<T>::point(point const & pt)
+{
+	this = pt;
+	// this->x = pt.getX();
+	// this->y = pt.getY();
+}
+
+template<typename T>
+inline T point<T>::getX()
+{
+	return T(this->x);
+}
+
+template<typename T>
+inline T point<T>::getY()
+{
+	return T(this->y);
+}
+
+template<typename T>
+inline void point<T>::setX(T a)
+{
+	this->x = a;
+}
+
+template<typename T>
+inline void point<T>::setY(T b)
+{
+	this->y = b;
+}
