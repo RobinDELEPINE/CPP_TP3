0dee7d9c3be6a0a44eebe25d6e486c452ce58407
Xquiratis <rom.pourias@gmail.com>
2022-12-07 16:56:29 +0100

    operator

--- CPP_TP3.cpp
+++ CPP_TP3.cpp
@@ -1,14 +1,12 @@
-// CPP_TP3.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 #include <iostream>
 #include "point.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
     point<int> pt1(12, 13);
     point<int> pt2(pt1);
+    std::cout << "pt1 : " << pt1 << "\n";
+    std::cout << "pt2 : " << pt2 << "\n";
+
 
 }
--- rectangle.h
+++ rectangle.h
@@ -1,22 +1,62 @@
+#ifndef _RECTANGLE_HPP_
+#define _RECTANGLE_HPP_
+
 #include <iostream>
 #include "point.h"
 
 template <typename T>
+class rectangle;
 
-class rectangle: private point<T>{
-   private:
-      T largeur;
-      T longeur;
-   public:
-      rectangle(point<T> p,T largeur, T longeur);
-      void translater(T a, T b);
-      bool contient(point<T>  p1);
-      void affiche(void);
+template <typename T>
+std::ostream& operator<<(std::ostream &, rectangle<T> const&);
 
+template <typename T>
+class rectangle{
+ 
+protected:
+   
+   //Les points du Rectangle
+   pointT<T> p ;
+   T l;
+   T h;
+
+public:
+    rectangle(pointT<T> p, T l, T h);
+    pointT<T> getPoint() const;
+    T getL() const;
+    T getH() const;
+
+   friend std::ostream& operator<< <T>(std::ostream &, rectangle const&);
+    
 };
 
-template<typename T>
-rectangle<T> :: rectangle(point<T> p, T largeur, T longeur) : point<T>(p.getX(), p.getY()){
-   this->largeur = largeur;
-   this->longeur = longeur;
+template <typename T>
+rectangle<T>::rectangle(pointT<T> p, T l, T h){
+   this->p = p; //attention copie superficielle
+   this->h = h;
+   this->l = l;
 }
+
+template <typename T>
+T rectangle<T>::getL() const{
+   return this->l;
+}
+
+template <typename T>
+T rectangle<T>::getH() const{
+   return this->h;
+}
+
+template <typename T> 
+pointT<T> rectangle<T>::getPoint() const{
+   return this->p;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream &o, rectangle<T> const &R)
+{
+   o<<"("<<R.getPoint().getX()<<","<<R.getPoint().getY()<<"), "<<R.getH()<<", "<<R.getL()<<std::endl;
+
+  return o;
+}
+#endif
